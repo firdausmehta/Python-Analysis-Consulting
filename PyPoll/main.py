b51f8a7be23437f32f5b1b3db1fc729d14249c8f
Firdaus Mehta <firmehta97@gmail.com>
2021-05-26 23:30:12 +1000

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,19 +17,25 @@ with open(csvpath, newline="") as csvfile:
         next(reader, None)
 
         for row in reader:
-     # start counting total votes    
-          
-    # get the reference to the candidate name from the row 
-         
-    # begin the if statement 
+            # start counting total votes    
+                   
+            # get the reference to the candidate name from the row 
+           
+            # begin the if statement 
+           
+            # add the candidate to the list_candidate 
 
-    # add the candidate to the list_candidate 
-    
     
     # print the total votes 
-
+    
     
     # loop through the candidates and calculate their percentage of the votes 
- 
+  
 
-    # print to terminal and the txt file  
+    # print to terminal and the txt file       
+    print(f"--------------------")
+    print(f"Winner: {winner}")
+    print(f"--------------------")
+    datafile.write(f"--------------------\n")
+    datafile.write(f"Winner: {winner}\n")
+    datafile.write(f"--------------------")
