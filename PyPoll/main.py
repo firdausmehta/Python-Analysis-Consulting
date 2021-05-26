281319f0ad9777336a3a2aae9554d4aa178fccd3
Firdaus Mehta <firmehta97@gmail.com>
2021-05-27 07:13:20 +1000

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,13 +18,23 @@ with open(csvpath, newline="") as csvfile:
 
         for row in reader:
             # start counting total votes    
-                   
+            total_votes += 1 
+        
             # get the reference to the candidate name from the row 
-            
+            candidate = row[2]
+
             # begin the if statement 
-          
+            if candidate not in candidate_list:
             # add the candidate to the list_candidate 
-                   
+                candidate_list.append(candidate)
+                candidate_dict[candidate] = 0
+
+            
+            candidate_dict[candidate] +=1 
+
+output_file = 'Analysis/election_results.txt'
+with open(output_file, "w", newline="") as datafile:
+    
     # print the total votes 
     print(f"Election Results")
     print(f"--------------------")
@@ -37,7 +47,16 @@ with open(csvpath, newline="") as csvfile:
     datafile.write(f"--------------------\n")
     
     # loop through the candidates and calculate their percentage of the votes 
- 
+    for candidate in candidate_dict: 
+        percentage = round(float(candidate_dict[candidate])/float(total_votes),2)
+
+        print(f"{candidate}: {percentage:.3%} ({candidate_dict[candidate]})\n")
+        datafile.write(f"{candidate}: {percentage:.3%} ({candidate_dict[candidate]})\n")
+
+        votes = candidate_dict[candidate]
+        if votes > winning_vote:
+            winning_vote = votes
+            winner = candidate
 
     # print to terminal and the txt file       
     print(f"--------------------")
