0bf938431f28483893ec88a66fe88ecf679cb21f
Firdaus Mehta <firmehta97@gmail.com>
2021-05-26 22:48:49 +1000

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,21 @@
-main
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+     # start counting total votes    
+          
+    # get the reference to the candidate name from the row 
+         
+    # begin the if statement 
+
+    # add the candidate to the list_candidate 
+    
+    
+    # print the total votes 
+
+    
+    # loop through the candidates and calculate their percentage of the votes 
+ 
+
+    # print to terminal and the txt file  
